495f60e2d22b47bf40859c9f60da1be3d448d50f
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-10-23 04:15:34 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Card } from "./assets/components/card";
+
+import "./css/App.css";
+
+function App() {
+  return (
+    <div className="grid place-items-center h-screen">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/components/card.tsx
@@ -0,0 +1,7 @@
+export function Card() {
+  return (
+    <div className="card">
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </div>
+  );
+}
